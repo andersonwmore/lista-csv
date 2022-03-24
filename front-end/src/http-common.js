bcc09df6072b5e7862f7315481c519aa662f2ec7
andersonwmore <andersonwmore@gmail.com>
2022-03-24 15:35:44 -0300

    atualiza gitignore

--- front-end/src/http-common.js
+++ front-end/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: 'https://nodeserver.tecnonex.com.br',
+   baseURL: 'https://seuservidoraqui',
   "Content-type": "application/x-www-form-urlencoded"
 });
 
